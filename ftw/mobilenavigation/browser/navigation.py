f810497c566f77bb08abc3d3d1e892685b677091
Julian Infanger <j.infanger@4teamwork.ch>
2013-04-12 08:30:19 +0200

    Only show elements in navigation which are not excluded from navigation.

--- ftw/mobilenavigation/browser/navigation.py
+++ ftw/mobilenavigation/browser/navigation.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from zope.publisher.browser import BrowserView
 
 
@@ -5,21 +6,38 @@ class UpdateMobileNavigation(BrowserView):
 
     def __call__(self):
         subnavi = '<ul>'
-        for obj in self.context.getFolderContents(full_objects=True):
+        for obj in self.sub_objects(self.context):
             subnavi += '<li class="%s"><a href="%s">%s</a></li>' % (
                 self.get_css_classes(obj),
                 obj.absolute_url(),
                 obj.Title())
         subnavi += '</ul>'
-
         return subnavi
 
+    def sub_objects(self, parent):
+        """ Returns the sub objects of a given parent.
+        Checks if the objects should be listed in navi.
+        """
+        objs = []
+        properties = getToolByName(self.context, 'portal_properties')
+        hidden_types = properties.navtree_properties.metaTypesNotToList
+        for brain in parent.getFolderContents():
+            if brain.portal_type not in hidden_types:
+                obj = brain.getObject()
+                if not obj.getExcludeFromNav():
+                    objs.append(obj)
+        return objs
+
     def get_css_classes(self, obj):
+        """ Returns classes for an navigation entry.
+        Checks if the content is folderish and have children.
+        Also adds the navigation depth.
+        """
         level = int(self.request.form.get('level', '1'))
         classes = []
 
         if not obj.isPrincipiaFolderish \
-                or len(obj.objectIds()) == 0 \
+                or len(self.sub_objects(obj)) == 0 \
                 or level >= 2:
             classes.append('noChildren')
         classes.append('level%s' % level)
