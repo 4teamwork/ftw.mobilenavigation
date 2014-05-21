d7dc4264d05ed912886a4ad291414483caa27c35
Jonas Baumann <jone@jone.ch>
2014-05-21 08:35:18 +0200

    4teamwork is an AG now.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='ftw.mobilenavigation',
         ],
 
       keywords='plone ftw content templates',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://github.com/4teamwork/ftw.mobilenavigation',
