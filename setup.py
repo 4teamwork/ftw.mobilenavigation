93f62f471e27a94247031d2ed696447db0e72a02
Martin Bächtold <martin@baechtold.me>
2017-12-22 13:11:38 +0100

    Drop support for Plone 4.2.

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(name='ftw.mobilenavigation',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Framework :: Plone :: 5.1',
         'Programming Language :: Python',
